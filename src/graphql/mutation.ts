75318d89d5a0d124bdd1ff2cb680f050670f3edb
MohammadRahman <rahmanmahbub0529@gmail.com>
2025-02-13 22:17:48 -0500

    Add a mutation that creates a todo item

--- src/graphql/mutation.ts
+++ src/graphql/mutation.ts
@@ -14,4 +14,20 @@ export const Mutation: IMutation<Context> = {
       name: something.name,
     };
   },
+  createTodo: async (_, { input }, { prisma }) => {
+    const todo = await prisma.todo.create({
+      data: {
+        title: input.title,
+        completed: false,
+      },
+    });
+
+    return {
+      id: todo.id,
+      title: todo.title,
+      completed: todo.completed,
+      createdAt: todo.createdAt.toISOString(),
+      updatedAt: todo.updatedAt.toISOString(),
+    };
+  },
 };
