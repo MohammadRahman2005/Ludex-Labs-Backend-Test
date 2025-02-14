503aa8eadc83e3924d1b4e1df72b2b91a4820543
MohammadRahman <rahmanmahbub0529@gmail.com>
2025-02-13 21:09:04 -0500

    Add GraphQL type definitions for creating todos

--- src/graphql/typeDefinitions.ts
+++ src/graphql/typeDefinitions.ts
@@ -3,16 +3,30 @@ export const typeDefs = /* GraphQL */ `
     name: String!
   }
 
+  input CreateTodoInput {
+    title: Strinng!
+  }
+
   type Something {
     id: ID!
     name: String!
   }
 
+  type Todo {
+    id: ID!
+    title: String!
+    completed: Boolean!
+    createdAt: String!
+    updatedAt: String!
+  }
+
   type Mutation {
     createSomething(input: CreateSomethingInput!): Something!
+    createTodo(input: CreateTodoInput!): Todo!
   }
 
   type Query {
     hello: String
+    todos: [Todo!]!
   }
 `;
